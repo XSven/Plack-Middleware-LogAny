59bf3630776b2e25de9d2f05ad401e680b527a77
Sven Willenbuecher <sven.willenbuecher@gmx.com>
2022-11-26 12:19:39 +0100

    tidying source code and test script
    apply .perltidyrc
    use explicit imports
    count tests in test script

--- lib/Plack/Middleware/LogAny.pm
+++ lib/Plack/Middleware/LogAny.pm
@@ -1,28 +1,30 @@
+#<<<
+use strict; use warnings;
+#>>>
+#
 package Plack::Middleware::LogAny;
 
-use Log::Any qw{};
-use Plack::Util::Accessor qw{category logger};
-use parent qw{Plack::Middleware};
-use strict;
-use warnings;
+use parent                qw( Plack::Middleware );
+use Log::Any              qw();
+use Plack::Util::Accessor qw( category logger );
 
 our $VERSION = '0.001001';
 
 sub prepare_app {
-    my ($self) = @_;
-    $self->logger (Log::Any->get_logger (category => $self->category || ''));
+  my ( $self ) = @_;
+  $self->logger( Log::Any->get_logger( category => $self->category || '' ) );
 }
 
 sub call {
-    my ($self, $env) = @_;
+  my ( $self, $env ) = @_;
 
-    $env->{'psgix.logger'} = sub {
-        my $args = shift;
-        my $level = $args->{level};
-        $self->logger->$level ($args->{message});
-    };
+  $env->{ 'psgix.logger' } = sub {
+    my $args  = shift;
+    my $level = $args->{ level };
+    $self->logger->$level( $args->{ message } );
+  };
 
-    $self->app->($env);
+  $self->app->( $env );
 }
 
 1;
